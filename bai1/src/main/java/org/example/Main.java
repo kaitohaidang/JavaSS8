35bc7ff1bbfff55753ceb29122561427973630b6
kaitohaidang <137318767+kaitohaidang@users.noreply.github.com>
2026-05-06 15:29:42 +0700

    add

---
+++ bai1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+         Student s =new Student(1,"hai dang",23);
+        System.out.println(s.getId()+" "+s.getName()+" "+s.getAge());
+        }
+    }
